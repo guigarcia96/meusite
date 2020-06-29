2332feb27fac6f9b9fdc1e3348a6d93048161a67
Guilherme Moraes <gmoraes1@outlook.com>
2020-06-29 18:13:50 -0300

    atualização do config.js 2.0

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://meublogdev.netlify.app/',
   pathPrefix: '/',
   title: 'Blog do Guilherme',
   subtitle: 'Meu Portfólio e Artigos',
@@ -23,7 +23,7 @@ module.exports = {
   ],
   author: {
     name: 'Guilherme Moraes',
-    photo: './public/photo.jpg',
+    photo: '/photo.jpg',
     bio: 'Eterno Estudante. Formado em Sistemas de Informação. DEV',
     contacts: {
       email: '',
